7f7638cd50c1ec8c89f73b7b231dcc8453a83229
k87938433-sudo <k87938433@gmail.com>
2025-12-02 23:54:44 +0200

    Add files via upload

---
+++ page.php
@@ -0,0 +1,25 @@
+<?php
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
+}
+get_header();
+?>
+<div class="site-content">
+    <main class="content-area">
+        <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+            <article id="post-<?php the_ID(); ?>">
+                <div class="breadcrumbs">
+                    <a href="<?php echo esc_url( home_url( '/' ) ); ?>">Главная</a> /
+                    <span><?php the_title(); ?></span>
+                </div>
+                <h1><?php the_title(); ?></h1>
+                <?php the_content(); ?>
+            </article>
+        <?php endwhile; else : ?>
+            <p>Страница не найдена.</p>
+        <?php endif; ?>
+    </main>
+    <?php get_sidebar(); ?>
+</div>
+<?php
+get_footer();
